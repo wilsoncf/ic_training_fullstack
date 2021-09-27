4877009662005e17f2ee069de01b8fb129e4939d
Wilson <wilsoncffilho@outlook.com>
2021-09-27 18:16:29 -0300

    Adicionando a funcionalidade de esqueceu a senha

---
+++ src/login/login.js
@@ -0,0 +1,8 @@
+function forgetPassword(){
+    document.getElementById("password").style.display="none";
+    document.getElementById("joinbtn").style.display="none";
+    document.getElementById("forget-password").style.display="none";
+   
+    document.getElementById("submitbtn").style.display="block";
+    document.getElementById("email-reset").style.display="block";
+}
